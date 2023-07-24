1084aa2419be9a8ac4596325c2ebafdfc99997c0
konvus22 <130019436+konvus22@users.noreply.github.com>
2023-07-24 15:58:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import openai
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+st.title('🦜🔗 Aplicación de inicio rápido')
+openai_api_key = st.sidebar.text_input('Clave API OpenAI')
+def generate_response(text_input):
+  llm = openai(temperatura=0.7,  openai_api_key=openai_api_key)
+  st.info(llm(text_input))
+with st.form('my_form'):
+    text = st.text_area('Introduzca texto:', '¿Cuáles son los tres consejos clave para aprender a programar?')
+    submitted = st.form_submit_button('Enviar')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Introduzca su clave API OpenAI', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
